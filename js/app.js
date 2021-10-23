b03c620f9762dcb137081869fae518846a43aba2
sauhard22 <sauhardsrivastava08@gmail.com>
2021-10-23 01:05:16 -0700

    search filter added

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
-function searchBlog(){
-    let filter = document.getElementById('filter');
+function search() {
+    let filter = document.getElementById('myInput').value.toUpperCase();
     let tableRecord = document.getElementById('tableRecord');
     let tr = tableRecord.getElementsByTagName('tr');
 
-    for(var i = 0; i< tr.length;i++){
+    for (var i = 0; i < tr.length; i++) {
         let td = tr[i].getElementsByTagName('td')[0];
-        if(td){
+        if (td) {
             let textvalue = td.textContent || td.innerHTML;
-            if(textvalue.toUpperCase().indexOf(filter) > -1){
+            if (textvalue.toUpperCase().indexOf(filter) > -1) {
                 tr[i].style.display = "";
-            }else{
+            } else {
                 tr[i].style.display = "none"
             }
-          
         }
     }
 }
